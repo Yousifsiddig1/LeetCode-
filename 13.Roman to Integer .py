300d9511b8d60ef86185c7965c0d2dd879605c83
Yousif <145899994+Yousifsiddig1@users.noreply.github.com>
2024-01-19 20:55:02 +0200

    13.Roman to Integer .py

--- 13.Roman to Integer .py
+++ 13.Roman to Integer .py
@@ -10,3 +10,10 @@ class Solution:
             "D": 500,
             "M": 1000
         }
+        num = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for i in s:
+            num += chars[i]
+        return num
