de92b448f950dc96a87937a9a77269b541db81b9
Yousif <145899994+Yousifsiddig1@users.noreply.github.com>
2024-01-23 22:49:22 +0200

    137. Single Number II .py

---
+++ 137. Single Number II .py
@@ -0,0 +1,7 @@
+https://leetcode.com/problems/single-number-ii/description/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in nums : 
+            if nums.count(i) == 1 : 
+                return  i
+            
