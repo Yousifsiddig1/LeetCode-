b6e9fc82cf933ce73e51742c377c9e6900a19a19
Yousif <145899994+Yousifsiddig1@users.noreply.github.com>
2024-01-23 22:52:29 +0200

    Medium Diffculty

--- 137. Single Number II .py
+++ 137. Single Number II .py
@@ -4,4 +4,19 @@ class Solution:
         for i in nums : 
             if nums.count(i) == 1 : 
                 return  i
+# Faster Solution
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        num_count = {}
+
+        # Count occurrences of each number
+        for num in nums:
+            num_count[num] = num_count.get(num, 0) + 1
+
+        # Find the number with count 1
+        for num, count in num_count.items():
+            if count == 1:
+                return num
+
             
