d511b03b019f4170bc882116cfe854dc534b3e18
Yousif <145899994+Yousifsiddig1@users.noreply.github.com>
2024-01-25 18:43:22 +0200

    2 solutions ( medium diff)

--- 80. Remove Duplicates from Sorted Array II.py
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -11,6 +11,18 @@ class Solution:
                 else:
                     k+=1
             i +=1
-        
+        return k
+# Better solution ( 66ms memory time )
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 0
+        i = 0
+        while i < len(nums):
+            if nums.count(nums[i]) > 2:
+                nums.pop(i)
+            else:
+                i += 1
+                k += 1
         return k
         
